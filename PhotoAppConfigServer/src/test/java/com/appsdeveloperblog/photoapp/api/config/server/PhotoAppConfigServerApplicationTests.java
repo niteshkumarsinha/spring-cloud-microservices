0f6abf4ca793c6215c15e50725ba1b6af000cc0f
NITESH KUMAR <nitesh111sinha@gmail.com>
2026-06-16 21:54:00 +0530

    Photo app microservices

---
+++ PhotoAppConfigServer/src/test/java/com/appsdeveloperblog/photoapp/api/config/server/PhotoAppConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.api.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
